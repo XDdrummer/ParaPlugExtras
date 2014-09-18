78f7931e167ecf58fb22e3a7bde0b4879ad70708
XDdrummer <cathamster7@gmail.com>
2014-09-18 11:49:07 -0400

    Update ParaPlugExtras.java

--- src/com/parapixelmon/xddrummer/paraplug/extras/ParaPlugExtras.java
+++ src/com/parapixelmon/xddrummer/paraplug/extras/ParaPlugExtras.java
@@ -1,16 +1,27 @@
 package com.parapixelmon.xddrummer.paraplug.extras;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import us.xddrummer.util.XDStrings;
 
 public class ParaPlugExtras extends JavaPlugin {
 
   @Override
   public void onEnable(){
     
+    XDStrings.log("Enabling ParaPlug Extras by XDdrummer. View Config.yml for details.");
+    
+    saveDefaultConfig();
+    XDStrings.log("Loaded config.yml in plugins>paraplugextras>config.yml");
+    
+    this.getCommand("setup-extras").setExecutor(SetupCommand(this));
+    XDStrings.log("Loaded /setup-extras");
+    
   }
   
   @Override
   public void onDisable(){
+    
+    XDStrings.log("Disabling ParaPlug Extras by XDdrummer. View config.yml for details.");
     
   }
 
