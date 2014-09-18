eb5a68381e10828ab8420f2303f3699a41f98fad
XDdrummer <cathamster7@gmail.com>
2014-09-18 11:53:02 -0400

    To prevent double-setups and data loss

---
+++ src/com/parapixelmon/xddrummer/paraplug/extras/StatStorage.java
@@ -0,0 +1,15 @@
+package com.parapixelmon.xddrummer.paraplug.extras;
+
+public class StatStorage {
+
+  public static class IsSetup {
+  
+    public static boolean isSetup;
+  
+    public static void setSetup(boolean is){
+      this.isSetup = is;
+    }
+  
+  }
+
+}
